712b9c3a8b91899d78d0cbcc4833a87b0ce3a459
Diego032396 <diegojosuegalvez@gmail.com>
2024-04-30 22:00:17 -0500

    ejemplo

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,7 +7,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new Vistas.vPersona();
             PersonRepo = personRepository;
         }
     }
