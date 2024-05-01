1f4177f1dd8d111d08aa3ab14a5183a6093bca96
Diego032396 <diegojosuegalvez@gmail.com>
2024-04-30 21:50:58 -0500

    Agregar archivos de proyecto.

---
+++ PersonRepository.cs
@@ -0,0 +1,62 @@
+﻿using dgalvezS5.Modelos;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dgalvezS5
+{
+    public class PersonRepository
+    {
+        string _dbPath;
+        private SQLiteConnection conn;
+
+        public string statusMessage {  get; set; }
+
+        public void Init()
+        { 
+            if (conn == null)
+                return;
+            conn = new(_dbPath);
+            conn.CreateTable<Persona> ();       
+        }
+        public PersonRepository (string dbPath)
+        { 
+            _dbPath = dbPath;
+        }
+
+        public void addNewPerson(string name)
+        {
+            int result = 0;
+            try
+            {
+                Init();
+                if (string.IsNullOrEmpty(name))
+                    throw new Exception("El nombre es requerido");
+                Persona persona = new () { Name = name };
+                result = conn.Insert (persona);
+                statusMessage = string.Format("Dato agreagado", result, name);
+            }
+            catch (Exception ex)
+            {
+                statusMessage = string.Format("Error no se inserto ", name, ex.Message);
+            }
+        }
+
+        public List<Persona> GetAllPeople()
+        {
+            try
+            {
+                Init();
+                return conn.Table<Persona>().ToList();
+            }
+            catch (Exception ex)
+            {
+                statusMessage = string.Format("Error al Mostrar los datos", ex.Message);
+            }
+            return new List<Persona>();
+        }
+    }
+}
---
+++ Vistas/vPersona.xaml.cs
@@ -0,0 +1,24 @@
+using dgalvezS5.Modelos;
+
+namespace dgalvezS5.Vistas;
+
+public partial class vPersona : ContentPage
+{
+	public vPersona()
+	{
+		InitializeComponent();
+	}
+
+    private void btnAgregar_Clicked(object sender, EventArgs e)
+    {
+       lblStatus.Text = "";
+        App.PersonRepo.addNewPerson(txtPersona.Text);
+        lblStatus.Text = App.PersonRepo.statusMessage;
+    }
+
+    private void btnObtener_Clicked(object sender, EventArgs e)
+    {
+        lblStatus.Text = "";
+        List<Persona> personas = new List<Persona>();
+    }
+}
